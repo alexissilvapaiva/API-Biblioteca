fdcc0ff2f067c481689a38d51add79d570e9b466
Alexis Paiva <lelviejox@gmail.com>
2023-12-05 19:31:39 -0300

    redundancia ciclica, comentario

--- APIBiblioteca/DTO/GeneroDTO.cs
+++ APIBiblioteca/DTO/GeneroDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace APIBiblioteca.DTO
+﻿using APIBiblioteca.Models;
+
+namespace APIBiblioteca.DTO
 {
     public class GeneroDTO
     {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        //causa redundancia cíclica, ingresa a "libro" >> dsp "genero" y asi
+        // sucesivamente
+        public HashSet<Libro> Libros { get; set; } = new HashSet<Libro>();
     }
 }
